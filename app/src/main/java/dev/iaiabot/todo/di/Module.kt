1d89f851f08ec5d159791e70dbd65ae5bbbcfc63
iaia <iaia72160@gmail.com>
2021-04-10 20:29:53 +0900

    追加

--- app/src/main/java/dev/iaiabot/todo/di/Module.kt
+++ app/src/main/java/dev/iaiabot/todo/di/Module.kt
@@ -2,11 +2,14 @@ package dev.iaiabot.todo.di
 
 import dev.iaiabot.todo.login.LoginViewModel
 import dev.iaiabot.todo.login.LoginViewModelImpl
+import dev.iaiabot.todo.task.TaskViewModel
+import dev.iaiabot.todo.task.TaskViewModelImpl
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 object Module {
     val appModule = module {
         viewModel<LoginViewModel> { LoginViewModelImpl(get()) }
+        viewModel<TaskViewModel> { TaskViewModelImpl(get(), get(), get()) }
     }
 }
