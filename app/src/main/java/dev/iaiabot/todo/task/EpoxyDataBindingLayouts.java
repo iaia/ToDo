676bd234802157a15308ab060fc993cc722d01bd
iaia <iaia72160@gmail.com>
2021-04-10 21:36:37 +0900

    Add list item layout

--- app/src/main/java/dev/iaiabot/todo/task/EpoxyDataBindingLayouts.java
+++ app/src/main/java/dev/iaiabot/todo/task/EpoxyDataBindingLayouts.java
@@ -2,7 +2,11 @@ package dev.iaiabot.todo.task;
 
 import com.airbnb.epoxy.EpoxyDataBindingLayouts;
 
+import dev.iaiabot.todo.R;
+
 @EpoxyDataBindingLayouts({
+        R.layout.list_item_add_task,
+        R.layout.list_item_task,
 })
 interface EpoxyConfig {
 }
