5494b6cd07fe254b4953091a2c50d0b1296c92d6
iaia <iaia72160@gmail.com>
2021-06-13 15:07:35 +0900

    Add signup fragment

---
+++ app/src/main/java/dev/iaiabot/todo/signup/SignUpFragment.kt
@@ -0,0 +1,37 @@
+package dev.iaiabot.todo.signup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dev.iaiabot.todo.databinding.FragmentSignUpBinding
+import dev.iaiabot.todo.observeToastAction
+import org.koin.android.ext.android.inject
+
+class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
+    private val viewModel: SignUpViewModel by inject()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+        viewLifecycleOwner.lifecycle.addObserver(viewModel)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.routerAction.observe(viewLifecycleOwner) {
+        }
+
+        observeToastAction(viewModel)
+    }
+}
