189b910a7755dd0450c8be2fcd68f0761de47272
iaia <iaia72160@gmail.com>
2021-06-19 23:35:05 +0900

    Switch edit mode

--- app/src/main/java/dev/iaiabot/todo/task/TaskController.kt
+++ app/src/main/java/dev/iaiabot/todo/task/TaskController.kt
@@ -4,23 +4,27 @@ import com.airbnb.epoxy.TypedEpoxyController
 import dev.iaiabot.entity.Task
 import dev.iaiabot.todo.databinding.ListItemTaskBinding
 import dev.iaiabot.todo.listItemTask
+import java.lang.ref.WeakReference
 
 class TaskController(
     private val viewModel: TaskViewModel
 ) : TypedEpoxyController<List<Task>>() {
+
+    var editModeTaskBinding: WeakReference<ListItemTaskBinding>? = null
 
     override fun buildModels(data: List<Task>) {
         data.sortedByDescending { it.order }.forEach { task ->
             var checked = task.completed
             listItemTask {
                 id(task.id)
-                task(task)
+                title(task.title)
                 editMode(false)
-                onBind { model, view, position ->
+                onBind { _, view, _ ->
                     val binding = view.dataBinding as ListItemTaskBinding
                     binding.taskCard.isChecked = task.completed
                     binding.taskCard.setOnClickListener {
-                        binding.editMode = !(binding.editMode ?: false)
+                        switchEditMode(binding)
+                        viewModel.onChangeTask(task, binding.title ?: "")
                     }
                     binding.taskCard.setOnLongClickListener {
                         viewModel.toggleComplete(task)
@@ -31,5 +35,18 @@ class TaskController(
                 }
             }
         }
+    }
+
+    private fun switchEditMode(taskBinding: ListItemTaskBinding) {
+        if (taskBinding.editMode == true) {
+            taskBinding.editMode = false
+            editModeTaskBinding = null
+            return
+        }
+        if (editModeTaskBinding != null) {
+            editModeTaskBinding?.get()?.editMode = false
+        }
+        taskBinding.editMode = true
+        editModeTaskBinding = WeakReference(taskBinding)
     }
 }
