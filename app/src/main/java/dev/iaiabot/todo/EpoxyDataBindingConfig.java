567d394f27feb345e4de4fe0280d07cb7a4fe4e5
iaia <iaia72160@gmail.com>
2021-06-20 13:47:34 +0900

    Refactoring

--- app/src/main/java/dev/iaiabot/todo/EpoxyDataBindingConfig.java
+++ app/src/main/java/dev/iaiabot/todo/EpoxyDataBindingConfig.java
@@ -1,8 +1,6 @@
-package dev.iaiabot.todo.task;
+package dev.iaiabot.todo;
 
 import com.airbnb.epoxy.EpoxyDataBindingLayouts;
-
-import dev.iaiabot.todo.R;
 
 @EpoxyDataBindingLayouts({
         R.layout.list_item_task,
